fc3332a145fb30fe9db56689b46913de9d07151a
juned123 <juned@frrole.com>
2020-10-06 03:34:17 +0530

    Updates

--- precache-manifest.6f7eef293bd067a78811f65523ee061a.js
+++ precache-manifest.6f7eef293bd067a78811f65523ee061a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c3aa534819925048ec911c3ebbabeb5",
+    "revision": "09eeb41a8b5ecc17d293cf60c7a8bcdb",
     "url": "/clinikk-task/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clinikk-task/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "b7eecf9543907803d610",
+    "revision": "85f1f2a81c38fafa1973",
     "url": "/clinikk-task/static/css/main.4fe54cdb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clinikk-task/static/js/2.8505d947.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b7eecf9543907803d610",
-    "url": "/clinikk-task/static/js/main.1e3e233c.chunk.js"
+    "revision": "85f1f2a81c38fafa1973",
+    "url": "/clinikk-task/static/js/main.0ae6bc52.chunk.js"
   },
   {
     "revision": "e6d8c785aa376571fd0a",
@@ -34,5 +34,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "3e81b4ba29d5c73ff4bff342895b8585",
     "url": "/clinikk-task/static/media/logout.3e81b4ba.svg"
+  },
+  {
+    "revision": "1f636581ffa4e1b4c6bb10294f1f4059",
+    "url": "/clinikk-task/static/media/subs.1f636581.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clinikk-task/precache-manifest.6604e1ce4ca7f9926e4db0d149158824.js"
+  "/clinikk-task/precache-manifest.6f7eef293bd067a78811f65523ee061a.js"
 );
 
 self.addEventListener('message', (event) => {
